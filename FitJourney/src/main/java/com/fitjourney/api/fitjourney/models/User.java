785e98adee99d7d78c66fa860b27bf9380ecb57c
IliyaKuzmanov <ilia.kuzmanov.a60@learn.telerikacademy.com>
2024-12-18 19:48:18 +0200

    add User entity

--- FitJourney/src/main/java/com/fitjourney/api/fitjourney/models/User.java
+++ FitJourney/src/main/java/com/fitjourney/api/fitjourney/models/User.java
@@ -1,4 +1,30 @@
 package com.fitjourney.api.fitjourney.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String phone;
 }
